a7e840e46030d7ed7a901c48b6fd62091356322e
Lukas Bestle <mail@lukasbestle.com>
2023-01-08 21:53:55 +0100

    Initial commit

---
+++ Ledger.lua
@@ -0,0 +1,397 @@
+-- MoneyMoney extension for transaction export in ledger format
+-- for text-based double-entry accounting
+-- https://github.com/lukasbestle/moneymoney-ledger
+--
+---------------------------------------------------------
+--
+-- MIT License
+--
+-- Copyright (c) 2023 Lukas Bestle
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+Exporter({
+	version = 1.00,
+	format = "Ledger",
+	fileExtension = "journal",
+	reverseOrder = true, -- export transactions in chronological order
+	description = MM.language == "de" and "Export von Umsätzen als ledger .journal-Datei"
+		or "Export transactions as ledger .journal file",
+	options = {
+		{
+			label = MM.language == "de" and "Umsätze müssen kategorisiert sein"
+				or "Transactions must have a category",
+			name = "needsCategory",
+			default = true,
+		},
+		{
+			label = MM.language == "de" and "Umsätze müssen als erledigt markiert sein"
+				or "Transactions must be checked",
+			name = "needsCheckmark",
+			default = true,
+		},
+	},
+})
+
+-- define local variables and functions
+---@type string[]
+local transactionErrors
+local extractRegex, formatTags, localizeText, parseCategory, parseTags, trim
+
+-----------------------------------------------------------
+
+---Writes the first line(s) of the export file;
+---called only once per export on the first account
+---
+---@param account Account Account from which transactions are being exported
+---@param startDate timestamp Booking date of the oldest transaction
+---@param endDate timestamp Booking date of the newest transaction
+---@param transactionCount integer Number of transactions to be exported
+---@param options table<string, boolean> Export options from the `Exporter` constructor
+---@return string? error Optional error message that aborts the export
+function WriteHeader(account, startDate, endDate, transactionCount, options)
+	-- find the decimal mark for the current locale (`.` or `,`)
+	-- by extracting it from a string formatted as `0.00` or `0,00`
+	local decimalMark = MM.localizeAmount(0):sub(2, 2)
+
+	assert(io.write("; Export: " .. os.date("%Y-%m-%d %H:%M:%S") .. "\n" .. "decimal-mark " .. decimalMark .. "\n"))
+
+	-- reset the error list from previous exports
+	transactionErrors = {}
+end
+
+---Writes a consecutive list of transactions of the same account;
+---called multiple times if transactions from multiple accounts are exported
+---
+---@param account Account Account from which transactions are being exported
+---@param transactions Transaction[] List of transactions of a booking date
+---@param options table<string, boolean> Export options from the `Exporter` constructor
+---@return string? error Optional error message that aborts the export
+function WriteTransactions(account, transactions, options)
+	local financialAccount = account.attributes.LedgerAccount or ("Assets:" .. account.name)
+
+	for _, transaction in ipairs(transactions) do
+		local transactionError
+
+		-- ensure that the transaction has a category
+		if transaction.category == "" then
+			transaction.category = "Unknown"
+
+			-- treat the missing category as an error if requested by the user
+			if options.needsCategory == true then
+				transactionError = localizeText(
+					"The transaction does not have an assigned category",
+					"Der Umsatz hat keine zugewiesene Kategorie"
+				)
+			end
+		end
+
+		-- ensure that the transaction is checked (if requested by the user)
+		if options.needsCheckmark == true and transaction.checkmark == false then
+			transactionError = transactionError
+				or localizeText("The transaction was not checked", "Der Umsatz ist nicht als erledigt markiert")
+		end
+
+		-- status character (with trailing space only when present
+		-- so that an empty character won't produce two spaces)
+		local statusCharacter = ""
+		if transaction.checkmark == true then
+			statusCharacter = "* "
+		elseif transaction.booked == false then
+			statusCharacter = "! "
+		end
+
+		-- extract the counter account and list of tags from the category
+		local counterAccountHierarchy, tags = parseCategory(transaction.category)
+		local counterAccount = table.concat(counterAccountHierarchy, ":")
+		if counterAccount == "" then
+			-- the category name is so invalid that it made the account name empty;
+			-- treat as high-priority error that overrides previous errors
+			transactionError = localizeText(
+				"Empty counter account from category '" .. transaction.category .. "'",
+				"Leeres Gegenkonto aus der Kategorie '" .. transaction.category .. "'"
+			)
+
+			-- continue with a placeholder name for the output file
+			counterAccount = "Invalid"
+		end
+
+		-- extract the comment and list of tags from the transaction comment
+		local comment, transactionTags = parseTags(transaction.comment:gsub("\n", " "), true)
+		if comment ~= "" then
+			transactionTags.comment = comment
+		end
+
+		-- convert the purpose text to a tag, but only if the
+		-- transaction comment did not override the purpose tag
+		if transaction.purpose ~= "" and transactionTags.purpose == nil then
+			transactionTags.purpose = transaction.purpose:gsub("\n", " ")
+		end
+
+		-- merge the transaction tags into the existing list
+		-- (with precendence of the more specific transaction tags)
+		for key, value in pairs(transactionTags) do
+			tags[key] = value
+		end
+
+		-- separate handling of the `[date]`, `{tax}` and `<code>` tags
+		-- as they will not be set as transaction tags but
+		-- as posting tags or in the transaction header
+		local postingTags = {}
+		local code = ""
+		if tags.date then
+			-- literal tag without key
+			table.insert(postingTags, tags.date)
+			tags.date = nil
+		end
+		if tags.tax then
+			postingTags.tax = tags.tax
+			tags.tax = nil
+		end
+		if tags.code then
+			code = "(" .. tags.code .. ") "
+			tags.code = nil
+		end
+
+		-- flip the amount as we print it for the counter account;
+		-- the format uses an ASCII space to enable commodity parsing in ledger/hledger
+		local amount = MM.localizeAmount("#,##0.00 ¤;-#,##0.00 ¤", -transaction.amount, transaction.currency)
+
+		-- assemble the transaction string
+		local output = (
+			os.date("%Y-%m-%d", transaction.bookingDate)
+			.. "="
+			.. os.date("%Y-%m-%d", transaction.valueDate)
+			.. " "
+			.. statusCharacter
+			.. code
+			.. transaction.name
+			.. formatTags(tags, "\n  ", "\n  ")
+			.. "\n  "
+			.. counterAccount:gsub("%s+", " ")
+			.. "  "
+			.. amount
+			.. formatTags(postingTags, "\n    ", " ")
+			.. "\n  "
+			.. financialAccount:gsub("%s+", " ")
+		)
+
+		-- handle a transaction error
+		if transactionError then
+			-- prepend the error to the output as comment;
+			-- convert the output to line comments to comment out the invalid transaction
+			output = ("; Error: " .. transactionError .. "\n" .. output):gsub("\n", "\n; ")
+
+			-- add transaction details to the user-facing error message
+			transactionError = string.format(
+				"%s:\n%s · %s (%s)",
+				transactionError,
+				MM.localizeDate(transaction.bookingDate),
+				transaction.name,
+				MM.localizeAmount(transaction.amount, transaction.currency)
+			)
+
+			-- collect the full error message in the global list for aggregate output
+			table.insert(transactionErrors, transactionError)
+		end
+
+		-- print the transaction to the export file;
+		-- leading newline so that there is an empty line before the
+		-- first transaction but also in between each transaction
+		assert(io.write("\n" .. output .. "\n"))
+	end
+end
+
+---Writes the last line(s) of the export file;
+---called only once per export on the first account
+---
+---@param account Account Account from which transactions are being exported
+---@param options table<string, boolean> Export options from the `Exporter` constructor
+---@return string? error Optional error message that aborts the export
+function WriteTail(account, options)
+	-- return error message if the list is non-empty
+	if next(transactionErrors) then
+		local message = localizeText(
+			"Incomplete export because of transaction errors:",
+			"Unvollständiger Export wegen Fehlern bei Umsätzen:"
+		)
+
+		return (message .. "\n\n" .. table.concat(transactionErrors, "\n\n"))
+	end
+end
+
+-----------------------------------------------------------
+
+---Extracts a value from the string by regular expression;
+---if the regex doesn't match, returns the string unaltered
+---
+---@param str string
+---@param regex string
+---@param multiple? boolean If `true`, all matches are returned as a list
+---@return string str Remaining string
+---@return string[] results Single or multiple matches
+---@overload fun(str: string, regex: string): string, string
+function extractRegex(str, regex, multiple)
+	local results = {}
+
+	for match in str:gmatch(regex) do
+		-- collect the matched value
+		table.insert(results, match)
+
+		-- find the position of the match in the string
+		-- while treating the match string as plain text
+		local i, j = str:find(match, nil, true)
+
+		-- remove the match from the string and ensure that there
+		-- is exactly one space where the match was to fill the gap
+		str = trim(trim(str:sub(0, i - 1)) .. " " .. trim(str:sub(j + 1)))
+	end
+
+	if multiple == true then
+		return str, results
+	end
+
+	return str, table.remove(results)
+end
+
+---Converts a list of tags into a ledger output string
+---
+---@param tags table<string | integer, string> List of preformatted tags or key-value table
+---@param separator string String to print in between tags
+---@param start? string String to print at the start of the returned string if tags are being printed
+---@return string
+function formatTags(tags, separator, start)
+	local tagStrings = {}
+
+	for key, value in pairs(tags) do
+		if type(key) == "number" then
+			-- print tag as is without using the key
+			value = "; " .. value
+		elseif value and value ~= "" then
+			value = "; " .. key .. ": " .. value
+		else
+			value = "; " .. key .. ":"
+		end
+
+		table.insert(tagStrings, value)
+	end
+
+	-- check if the list is non-empty
+	if next(tagStrings) then
+		return (start or "") .. table.concat(tagStrings, separator)
+	else
+		return ""
+	end
+end
+
+---Returns the string in the current UI language
+---
+---@param en string English text
+---@param de string German text
+function localizeText(en, de)
+	return MM.language == "de" and de or en
+end
+
+---Extracts tags from a hierarchical category name and
+---converts the labels or optionally the overridden
+---`[name]` into an account name hierarchy
+---
+---@param name string
+---@return string[] hierarchy List of the account name levels
+---@return table<string, string> tags Extracted tags
+function parseCategory(name)
+	-- extract the tags first, which will ensure that
+	-- tags defined in lower levels override higher ones
+	local tags
+	name, tags = parseTags(name)
+
+	-- collect each level that will be used
+	local hierarchy = {}
+
+	for levelName in name:gmatch("([^\\]+)") do
+		-- extract the `[name]` tag
+		local nameOverride
+		levelName, nameOverride = extractRegex(levelName, "%[.-%]")
+
+		if nameOverride then
+			-- there was a `[name]` tag
+
+			if nameOverride ~= "[]" then
+				-- it is non-empty, so replace the hierarchy with it
+				hierarchy = { nameOverride:match("%[(.-)%]") }
+			end
+
+		-- otherwise this hierarchy level will be skipped
+		else
+			-- there was no `[name]` tag, use the plain name
+			table.insert(hierarchy, trim(levelName))
+		end
+	end
+
+	return hierarchy, tags
+end
+
+---Extracts the `{tax}` and `#custom` tags from a string
+---
+---@param str string
+---@param transaction? boolean If `true`, the `<code>` and `[date]` tags are extracted as well
+---@return string str Remaining string
+---@return table<string, string> tags Extracted tags
+function parseTags(str, transaction)
+	local tags = {}
+	local result
+
+	-- transaction-specific tags
+	if transaction == true then
+		-- `<code>` tag
+		str, result = extractRegex(str, "<.->")
+		if result then
+			tags.code = result:match("<(.-)>")
+		end
+
+		-- `[date]` tag
+		str, result = extractRegex(str, "%[.-%]")
+		if result then
+			tags.date = result
+		end
+	end
+
+	-- `{tax}` tag
+	str, result = extractRegex(str, "{.-}")
+	if result then
+		tags.tax = result:match("{(.-)}")
+	end
+
+	-- `#custom` tags (parsed last to allow using the `#` inside other tags)
+	str, result = extractRegex(str, "#[^%s]+", true)
+	for _, tag in ipairs(result) do
+		local name, value = tag:match("^#([^:]+):?(.*)$")
+		tags[name] = value
+	end
+
+	return str, tags
+end
+
+---Removes spaces from the beginning and end of a string
+---
+---@param str string
+---@return string
+function trim(str)
+	return str:match("^%s*(.-)%s*$")
+end
---
+++ README.md
@@ -0,0 +1,101 @@
+# Ledger extension for MoneyMoney
+
+This extension for [MoneyMoney](https://moneymoney-app.com/) exports transactions in the [ledger](https://www.ledger-cli.org)/[hledger](https://hledger.org) format for text-based double-entry accounting.
+
+## Export format
+
+The extension creates a text file in ledger `.journal` format. The used syntax is fully compatible with both ledger and hledger:
+
+```ledger
+; Export: 2042-04-02 10:04:00
+decimal-mark ,
+
+2042-04-02=2042-04-01 * (2042-0042.pdf) Some Client
+  ; purpose: Invoice no. 2042-0042
+  ; comment: A nice comment
+  ; customtag:
+  Income:Revenue  -1.190,00 € ; [=2042-03-20]
+    ; tax: 19%
+  Assets:Bank
+```
+
+## How to use
+
+### Installation
+
+1. Download the signed extension file of the [latest release](https://github.com/lukasbestle/moneymoney-ledger/releases/latest).
+2. Open the database directory of MoneyMoney as described on the [MoneyMoney website](https://moneymoney-app.com/extensions/).
+3. Place the extension file (`Ledger.lua`) in the `Extensions` directory of your MoneyMoney database.
+
+### Export process
+
+To export your MoneyMoney transactions to the ledger format, select one or more transactions, right-click and select "Export Transactions..." (or use the same option in the "Account" menu). In the format selector, choose "Ledger (.journal)".
+
+By default, the extension validates that each transaction in MoneyMoney was assigned to a category and has the checkmark (menu "View > Checkmarks") checked. The selected category will be used as the counter account in the double-entry transaction. The checkmark allows you to confirm every transaction inside MoneyMoney before you export it to your accounting setup.
+
+If you don't need these safety checks, you can disable them with the checkboxes in the export dialog:
+
+- If you disable the category check, transactions without an assigned category will be booked to an account named `Unknown`.
+- If you disable the checkmark check, transactions without the checkmark are exported with an empty status character (or a `!` sign if the transaction is pending).
+
+Transactions that cause an export error because of the safety checks or because of invalid customization options are printed in commented-out form.
+
+## Input format and customization
+
+The ledger format is very rich. While MoneyMoney has good metadata support, some accounting metadata cannot be recorded natively. However you can use the following, fully optional text syntax in category names and transaction comments. This syntax will then be automatically converted on export.
+
+### Financial account
+
+The **name of the financial account** in ledger will be generated from the name of your bank account in MoneyMoney by default. So if your bank account is called "Giro", the generated ledger account name will be "Assets:Giro".
+
+You can optionally customize the ledger account name with a user-defined field in MoneyMoney. To do this, right-click on your account in the MoneyMoney sidebar, open the account settings and switch to the "Notes" page. There you will find a table for user-defined fields. Create a new user-defined field with the attribute name `LedgerAccount` and set it to your custom account name:
+
+<img alt="Screenshot of the LedgerAccount option in the MoneyMoney user-defined fields table" src="options.png" width="574">
+
+The custom account name does *not* have to start with "Assets:".
+
+### Category
+
+The counter account of each transaction is determined by the selected MoneyMoney category.
+
+The **name of the counter account** in ledger will be generated from the MoneyMoney category hierarchy. So a category `Expenses > Travel > Accommodation` (with `>` representing the hierarchy) becomes the account `Expenses:Travel:Accommodation`. If you need to customize the account name, you can use square brackets in the name of your categories or category groups:
+
+- Adding empty square brackets `[]` into the name makes that nesting level disappear. This can be used to:
+  - organize your categories inside MoneyMoney (e.g. `My Business [] > Expenses > Travel` becomes just `Expenses:Travel`) or to
+  - create catch-all categories that will use the category group as an account name (e.g. `Expenses > Travel > Etc. []` becomes just `Expenses:Travel`).
+- Adding non-empty square brackets overrides the name of the current and all higher hierarchy levels. Examples:
+  - `My Business > Travel [Expenses:Travel] > Accommodation` becomes `Expenses:Travel:Accommodation`.
+  - `Travel > Accommodation [1234]` becomes `1234` (useful if your ledger accounts are aliased).
+
+Each category can additionally have two different types of tags:
+
+- You can define a **tax tag** with the `{tax}` syntax. E.g. `{19%}` becomes the output `CounterAccount  123.00 € ; tax: 19%`. The tax tag is always set directly on the posting of the counter account, not on the whole transaction (because the tax does not apply to the financial account).
+- You can define one or multiple **custom tags** with the `#tag` or `#tag:value` syntax. The name and value may contain every character except whitespace and the comma (which is used to separate tags in hledger). The name may not contain the colon (which is the value separator in both ledger and hledger). Custom tags are set on the whole transaction.
+
+All tags can be defined on any category hierarchy level (on a category group or on the category directly). Values defined higher up are inherited but overridden by values defined below.
+
+### Transaction
+
+Each individual MoneyMoney transaction provides a lot of native metadata. The goal of this extension is to export all information that can be useful for accounting:
+
+The **transaction dates** (booking date and value date) are used as the two dates in the ledger transaction. If you specifically want to override the date of the counter posting, you can use the `[date1=date2]` syntax in the "Comment" field of the MoneyMoney transaction (right-click, "Add Comment..."). Both dates are optional, so you can use `[date1]` to just override the booking date and `[=date2]` to just override the value date. Overriding the dates can be useful e.g. to record invoice dates.
+
+The **transaction status** is stored in the character after the dates in the first line of the ledger transaction. Possible values are `*` for a cleared transaction (checkmark in MoneyMoney is checked), `!` for a pending transaction (displayed with striped background in MoneyMoney) and no character for an unmarked transaction (neither pending nor cleared).
+
+The **recipient name** is used as the payee in the first line of the transaction.
+
+If the "Comment" field of the MoneyMoney transaction (right-click, "Add Comment...") contains the `<code>` syntax, its value is stored as the transaction code in parentheses. This can be used to store the **file name of the associated receipt or invoice**. The value in between the angle brackets can be whatever you require.
+
+**All other tags** (`{tax}` tags and `#custom` tags) from the category name or the "Comment" field of the transaction are stored as tags. If a tag is set both in the category and in the transaction, the definition in the transaction has precedence, otherwise the definition from the category is inherited. You can read more about the tag syntax in the [category section](#category).
+
+If the "Comment" field of the MoneyMoney transaction contains additional text that is not detected as special syntax (`[dates]`, `<code>`, `{tax}` or `#custom` tags), the **comment** will be stored as a `; comment: ...` tag. If the comment contains multiple lines, the lines are concatenated with a single space.
+
+The **purpose text** of the transaction is stored as a `; purpose: ...` tag. If the purpose contains multiple lines, the lines are concatenated with a single space.
+
+The **account names** are generated based on the [bank account](#bank-account) and [category](#category) settings.
+
+The **amount** is taken from the MoneyMoney transaction and formatted according to the system locale. Please note that the sign of the amount is intentionally flipped because the amount you see is the amount of the *counter posting* (the mirror value of the financial posting). The amount of the financial posting is left blank because ledger/hledger automatically balance the transaction.
+
+## Credits
+
+The text syntax for category names and transaction comments and the export options were inspired by the [MoneyMonkey](https://github.com/timpritlove/moneymonkey) extension by Tim Pritlove.

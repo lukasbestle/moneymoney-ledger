fefe75fe38427228ad67c66d0281528dbf698ea0
Lukas Bestle <mail@lukasbestle.com>
2025-05-25 12:15:33 +0200

    Update version number

--- Ledger.lua
+++ Ledger.lua
@@ -27,7 +27,7 @@
 -- SOFTWARE.
 
 Exporter({
-    version = 1.10,
+    version = 1.11,
     format = "Ledger",
     fileExtension = "journal",
     reverseOrder = true, -- export transactions in chronological order
